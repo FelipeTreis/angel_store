9e23ada9dc544f61aea41e7d1f67333765346dc4
Felipe Treis <felipetreis1@gmail.com>
2022-07-26 10:48:43 -0300

    New styles\

--- angel/admin.py
+++ angel/admin.py
@@ -5,11 +5,17 @@ from angel.models import Category, Clothes
 
 @admin.register(Clothes)
 class ClothesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('is_published', 'id', 'title', 'created_at')
+    list_display_links = ('title',)
+    search_fields = ('id', 'title', 'category', 'gender', 'description',)
+    list_filter = ('category', 'is_published', 'gender',)
+    list_per_page = 10
+    list_editable = ('is_published',)
+    prepopulated_fields = {"slug": ('title',)}
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ('category',)
 
 
 admin.site.register(Category, CategoryAdmin)
